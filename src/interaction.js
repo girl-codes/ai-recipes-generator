d3f307d91a6081219be1c66f3e99dab2a9ef22cb
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-05-13 18:14:40 +0100

    Improved the user experience
    - Improved the UX in a few different ways including 'blink' method and hiding the response container until user has requested for recipe of a certain food / drink

--- src/interaction.js
+++ src/interaction.js
@@ -14,8 +14,12 @@ function generateRecipe(event) {
   let apiKey = "f12c79ob5a3b7240208d7843e2380dt7";
   let apiPrompt = `User instructions: Generate a recipe for ${instructionsInput.value}`;
   let apiContext =
-    "You are a world renowned chef and love sharing your delicious recipes. Your task is to generate the simplest recipe for whichever food or dish the user requests. Please seperate the heading, ingredients, and instructions in seperate sections. Make sure to follow the user instructions.Thank you! ";
+    "You are a world renowned chef and love sharing your delicious recipes. Your task is to generate the a simple recipe for whichever food or drink the user requests. Please make sure to seperate the heading, ingredients, and instructions into seperate sections. Make sure to follow the user instructions.Thank you! ";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="blink">Generating a simple recipe for ${instructionsInput.value}. Please be patient, our AI is doing their best 🤖 </div>`;
 
   axios.get(apiURL).then(displayRecipe);
 }
